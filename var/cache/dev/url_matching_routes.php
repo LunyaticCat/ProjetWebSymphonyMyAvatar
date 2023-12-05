ce66eb5af6d174eacbefe1fd5f6aac286ee2e4d1
pujadej <pujadej@K143-15.dep-info.iutmontp.univ-montp2.fr>
2023-12-05 10:05:55 +0100

    Retrait .env + MAJ .gitignore + Correction création controller

--- var/cache/dev/url_matching_routes.php
+++ var/cache/dev/url_matching_routes.php
@@ -14,6 +14,7 @@ return [
         '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
         '/_profiler/xdebug' => [[['_route' => '_profiler_xdebug', '_controller' => 'web_profiler.controller.profiler::xdebugAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
+        '/user/my/avatar' => [[['_route' => 'app_user_my_avatar', '_controller' => 'App\\Controller\\UserMyAvatarController::index'], null, null, null, false, false, null]],
     ],
     [ // $regexpList
         0 => '{^(?'
